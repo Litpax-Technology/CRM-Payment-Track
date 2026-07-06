94f65b70b009cdb1f22ed1c77d645b059fc25b1f
Litpax-Technology <mis@litpaxtechnology.com>
2026-07-06 15:12:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
  * Deploy → New deployment → Web app karne ke baad jo URL milta hai,
  * bilkul wahi (aakhir me /exec ke saath) yaha paste karo.
  */
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwewM6UqJdQrxSlZGZinDfdaMzFnGXqp_W2scmTbKpYzhCgJVy_aRDxAH1IOYismYJStQ/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwUo_BaSOWVPUi1w_Tk8z-xhfp8PA_zojkVj0yaF3VuWaVcC3LspGX-QcQLycgDynHJlA/exec';
